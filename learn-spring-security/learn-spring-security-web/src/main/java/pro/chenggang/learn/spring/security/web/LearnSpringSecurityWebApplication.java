96a8bc220de7bff43deca1038107c929d8b41cc1
Cheng Gang <chenggangpro@gmail.com>
2019-02-15 16:14:31 +0800

    spring security project init

---
+++ learn-spring-security/learn-spring-security-web/src/main/java/pro/chenggang/learn/spring/security/web/LearnSpringSecurityWebApplication.java
@@ -0,0 +1,12 @@
+package pro.chenggang.learn.spring.security.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringSecurityWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearnSpringSecurityWebApplication.class, args);
+  }
+}
---
+++ learn-spring-security/learn-spring-security-web/src/test/java/pro/chenggang/learn/spring/security/web/LearnSpringSecurityWebApplicationTests.java
@@ -0,0 +1,14 @@
+package pro.chenggang.learn.spring.security.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnSpringSecurityWebApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
